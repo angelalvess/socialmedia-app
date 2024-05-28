019282c6b8ddafc03d9ae73c9c97676e1b4488b5
angie <angeltrashangel@gmail.com>
2024-05-28 18:06:35 -0300

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-screen w-full flex-col bg-red-500">
+        <h1 className="underline">Oie testando</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
