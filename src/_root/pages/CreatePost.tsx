265a54408fef560e709f343c935fddacc50928c4
angelalvess <angeltrashangel@gmail.com>
2024-06-02 02:08:57 -0300

    feat: Add react-dropzone dependency and implement file uploader component

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import PostForm from '@/components/forms/PostForm'
 
 const CreatePost = () => {
-  return <div>CreatePost</div>
+  return (
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="flex-start w-full max-w-5xl justify-start gap-3">
+          <img
+            src="/assets/icons/add-post.svg"
+            alt="post image"
+            width={36}
+            height={36}
+          />
+          <h2 className="h3-bold md:h2-bold w-full text-left">Create Post</h2>
+        </div>
+
+        <PostForm />
+      </div>
+    </div>
+  )
 }
 
 export default CreatePost
